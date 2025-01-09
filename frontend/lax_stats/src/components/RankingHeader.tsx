c6bbaafee698a5c9d2db6ded0841003e9ae045a2
Jason Oh <jasonoh@utexas.edu>
2025-01-09 17:11:18 -0600

    wip: tooltip for ratings

--- frontend/lax_stats/src/components/RankingHeader.tsx
+++ frontend/lax_stats/src/components/RankingHeader.tsx
@@ -1,4 +1,9 @@
 import "./RankingHeader.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React, { useState, useEffect } from "react";
+import { Tooltip } from "bootstrap";
+
 
 interface Props{
 	league: String;
@@ -7,13 +12,32 @@ interface Props{
 	title3: String;
 }
 
+
 const RankingHeader = ({league, title1, title2, title3} : Props) => {
+
+  useEffect(() => {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(
+      (tooltipTriggerEl) => new Tooltip(tooltipTriggerEl)
+    );
+
+    // Clean up tooltips on unmount
+    return () => {
+      tooltipList.forEach((tooltip) => tooltip.dispose());
+    };
+  }, []);
+
+  // data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Tooltip on top"
+
   return (
     <div className="list-group-item ranking-header">
       <div className="ranking-title">{league}</div>
       <div className="stats-titles">
         <span>{title1}</span>
-        <span>{title2}</span>
+        <div>
+          <span>{title2}</span>
+          <i className="bi bi-info-circle"></i>
+        </div>
         <span>{title3}</span>
       </div>
     </div>
