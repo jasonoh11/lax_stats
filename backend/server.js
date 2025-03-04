99524e716258128b4011f400a4581266369fc999
Jason Oh <jasonoh@utexas.edu>
2025-03-04 09:12:37 -0600

    added connection pooling

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,13 @@ app.use(cors());
 
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-
+const db = mysql.createPool({
+	connectionLimit: 5,
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME
   });
-
-
-db.connect((err) => {
-	if (err) {
-		console.log(process.env.DB_HOST);
-		console.error('Database connection failed:', err.stack);
-	} else {
-		console.log('Connected to database');
-	}
-});
 
 app.get('/api/games/', (req, res) => {
 	var query = `SELECT * FROM games LIMIT 100`;
